057b8e1285f70ce39d21a2c8094d0f952aed695a
Marco Chávez <totto_cha@hotmail.com>
2016-05-31 23:56:49 -0600

    Almacenar valores de campos y una tabla y almacenarlos en un arreglo

---
+++ src/Logica/Operaciones.java
@@ -0,0 +1,16 @@
+
+package Logica;
+
+import Objeto.Dato;
+import Objeto.Tabla;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author u
+ */
+public class Operaciones {
+    public static ArrayList<Dato> listaDato = new ArrayList<>();
+    public static ArrayList<Tabla> listaTabla = new ArrayList<>();
+    
+}
---
+++ src/Objeto/Dato.java
@@ -0,0 +1,38 @@
+
+package Objeto;
+
+/**
+ *
+ * @author u
+ */
+public class Dato {
+    private String text1, text2, rbtn;
+
+    public Dato() {
+    }
+    
+    public String getText1() {
+        return text1;
+    }
+
+    public void setText1(String text1) {
+        this.text1 = text1;
+    }
+
+    public String getText2() {
+        return text2;
+    }
+
+    public void setText2(String text2) {
+        this.text2 = text2;
+    }
+
+    public String getRbtn() {
+        return rbtn;
+    }
+
+    public void setRbtn(String rbtn) {
+        this.rbtn = rbtn;
+    }
+
+}
